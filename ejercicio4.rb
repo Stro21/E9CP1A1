1125c67fcd8fbbec75656bfd437439308d591b35
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-17 20:30:14 -0300

    Se completó el ejercicio 4.

--- ejercicio4.rb
+++ ejercicio4.rb
@@ -1,6 +1,19 @@
 # Clase Dog
 class Dog
+  attr_reader :nombre, :raza, :color
+  def initialize(hash)
+    @nombre = hash[:nombre]
+    @raza = hash[:raza]
+    @color = hash[:color]
+  end
 
+  def print_dog
+    puts @nombre
+    puts @raza
+    puts @color
+  end
 end
 
-propiedades = {nombre: 'Beethoven', raza: 'San Bernardo', color: 'Café'}
+propiedades = { nombre: 'Beethoven', raza: 'San Bernardo', color: 'Café' }
+perro = Dog.new(propiedades)
+perro.print_dog
