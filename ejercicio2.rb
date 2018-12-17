55e7da01fe40aca8c314c5aa211c14f81ae3a3cc
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-17 20:02:24 -0300

    Se completo el ejercicio 2.

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -1,6 +1,10 @@
-class Car(model, year)
-  @model = model
-  @year = year
+# Clase auto
+class Car
+  attr_reader :model, :year
+  def initialize(model, year)
+    @model = model
+    @year = year
+  end
 end
 car = Car.new('Camaro', 2016)
 puts "Mi auto favorito es un #{car.model} del año #{car.year}!"
