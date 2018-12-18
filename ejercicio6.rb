2e5e7e1717e3a665ba5f788e83f1ec4f663c2dc8
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-17 21:59:34 -0300

    avance del ejercicio 6.

--- ejercicio6.rb
+++ ejercicio6.rb
@@ -1,5 +1,6 @@
 # Class Product
 class Product
+  attr_reader :name, :large, :medium, :small, :xsmall
   def initialize(name, large, medium, small, xsmall)
     @name = name
     @large = large
@@ -7,12 +8,18 @@ class Product
     @small = small
     @xsmall = xsmall
   end
+
+  def promedio
+    prom = (large + medium + small + xsmall) / 4
+    prom
+  end
 end
 products_list = []
 data = []
 File.open('catalogo.txt', 'r') { |file| data = file.readlines }
 data.each do |prod|
   ls = prod.split(', ')
-  products_list << Product.new(ls[0], ls[1], ls[2], ls[3], ls[4])
+  products_list << Product.new(ls[0], ls[1].to_i, ls[2].to_i, ls[3].to_i, ls[4].to_i)
 end
 puts products_list
+products_list.each { |product| puts product.promedio }
