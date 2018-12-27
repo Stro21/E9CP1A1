d5843f54563dc26f5c28ce8c04ab53bd45cb336b
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-26 21:53:44 -0300

    Se comprimió mas ejercicio el ejercicio 6.

--- ejercicio6.rb
+++ ejercicio6.rb
@@ -1,14 +1,12 @@
-# rubocop: disable LineLength
-
 # Class Product
 class Product
   attr_reader :name, :large, :medium, :small, :xsmall
-  def initialize(name, large, medium, small, xsmall)
-    @name = name
-    @large = large
-    @medium = medium
-    @small = small
-    @xsmall = xsmall
+  def initialize(*arr)
+    @name = arr[0]
+    @large = arr[1].to_i
+    @medium = arr[2].to_i
+    @small = arr[3].to_i
+    @xsmall = arr[4].to_i
   end
 
   def promedio
@@ -20,9 +18,6 @@ data = []
 File.open('catalogo.txt', 'r') { |file| data = file.readlines }
 data.each do |prod|
   ls = prod.split(', ').map(&:chomp)
-  name, large, medium, small, xsmall = ls
-  products_list.push(Product.new(name, large.to_i, medium.to_i, small.to_i, xsmall.to_i))
+  products_list.push(Product.new(*ls))
 end
 products_list.each { |producto| puts producto.promedio }
-
-# rubocop: enable LineLength
